0127bd0ad16ee0c6bff6adefa3021b9f3a540bce
mgrinko <glukym@gmail.com>
2019-02-21 10:45:10 +0200

    PhonesCatalog is now a functional component

--- src/phones/PhonesCatalog.js
+++ src/phones/PhonesCatalog.js
@@ -1,36 +1,36 @@
 import React from 'react';
 
-export default class PhonesCatalog extends React.Component {
-  render() {
-    return (
-      <ul className="phones">
-        { this.props.phones.map(phone => (
+const PhonesCatalog = ({ phones }) => {
+  return (
+    <ul className="phones">
+      { phones.map(phone => (
 
-          <li className="thumbnail" key={phone.id}>
-            <span>### { phone.age }</span>
+        <li className="thumbnail" key={phone.id}>
+          <span>### { phone.age }</span>
 
-            <a className="thumb" href={'#' + phone.id}>
-              <img
-                alt={ phone.name }
-                src={ phone.imageUrl }
-              />
-            </a>
+          <a className="thumb" href={'#' + phone.id}>
+            <img
+              alt={ phone.name }
+              src={ phone.imageUrl }
+            />
+          </a>
 
-            <div className="phones__btn-buy-wrapper">
-              <button className="btn btn-success">
-                Add
-              </button>
-            </div>
+          <div className="phones__btn-buy-wrapper">
+            <button className="btn btn-success">
+              Add
+            </button>
+          </div>
 
-            <a href={'#' + phone.id}>
-              { phone.name }
-            </a>
+          <a href={'#' + phone.id}>
+            { phone.name }
+          </a>
 
-            <p>{ phone.snippet }</p>
-          </li>
+          <p>{ phone.snippet }</p>
+        </li>
 
-        ))}
-      </ul>
-    );
-  }
-}
+      ))}
+    </ul>
+  );
+};
+
+export default PhonesCatalog;
