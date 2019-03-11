ab8b93ddf1ceaad265439cc59e1ee432b9a56065
mgrinko <glukym@gmail.com>
2019-03-11 10:55:31 +0200

    Router was added to filter component

--- src/PhonesPage/Filter.js
+++ src/PhonesPage/Filter.js
@@ -1,45 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
-class Filter extends React.Component {
-  static propTypes = {
-    query: PropTypes.string,
+import debounce from 'lodash/debounce';
+
+const Filter = ({ location, history }) => {
+  const params = new URLSearchParams(location.search);
+  const query = params.get('query') || '';
+  const orderBy = params.get('orderBy') || Filter.ORDER_BY_AGE;
+
+  const handleQueryChange = debounce((event) => {
+    params.set('query', event.target.value);
+
+    history.push({
+      pathname: location.pathname,
+      search: params.toString(),
+    });
+  }, 500);
+
+  const handleOrderChanged = (event) => {
+    params.set('orderBy', event.target.value);
+
+    history.push({
+      pathname: location.pathname,
+      search: params.toString(),
+    });
   };
 
-  static defaultProps = {
-    query: '',
-  };
 
-  render() {
-    const { onQueryChanged, onOrderChanged } = this.props;
-    const { query, orderBy } = this.props;
+  return (
+    <div className="Filter">
+      <input
+        type="text"
+        value={query}
+        onChange={handleQueryChange}
+      />
 
-    return (
-      <div className="Filter">
-        <input
-          type="text"
-          value={query}
-          onChange={event => {
-            onQueryChanged(event.target.value);
-          }}
-        />
+      <select
+        name="orderBy"
+        value={orderBy}
+        onChange={handleOrderChanged}
+      >
+        <option value={Filter.ORDER_BY_AGE}>by Age</option>
+        <option value={Filter.ORDER_BY_NAME}>by Name</option>
+      </select>
+    </div>
+  );
+};
 
-        <select
-          name="orderBy"
-          value={orderBy}
-          onChange={event => {
-            onOrderChanged(event.target.value);
-          }}
-        >
-          <option value={Filter.ORDER_BY_AGE}>by Age</option>
-          <option value={Filter.ORDER_BY_NAME}>by Name</option>
-        </select>
-      </div>
-    );
-  }
-}
+Filter.propTypes = {
+  query: PropTypes.string,
+};
 
 Filter.ORDER_BY_AGE = 'age';
 Filter.ORDER_BY_NAME = 'name';
 
-export default Filter;
+export default withRouter(Filter);
