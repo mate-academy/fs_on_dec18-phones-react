6267a4c56066c53195812e7832d737c62274004e
mgrinko <glukym@gmail.com>
2019-03-29 10:36:17 +0200

    123

--- src/components/Root.js
+++ src/components/Root.js
@@ -3,14 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from '../App';
-import { ShoppingCartProvider } from '../ShoppingCartContext';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <ShoppingCartProvider>
-        <App />
-      </ShoppingCartProvider>
+      <App />
     </BrowserRouter>
   </Provider>
 );
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,24 +1,28 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+
 import messageReducer from '../ducks/message';
 import shoppingCartReducer from '../ducks/shoppingCart';
 
-const rootReducer = (state = {}, action) => {
-  return {
-    message: messageReducer(state.message, action),
-    shoppingCart: shoppingCartReducer(state.shoppingCart, action)
-  };
-};
+// const rootReducer = (state = {}, action) => {
+//   return {
+//     message: messageReducer(state.message, action),
+//     shoppingCart: shoppingCartReducer(state.shoppingCart, action)
+//   };
+// };
 
-// const rootReducer = combineReducers({
-//   message: messageReducer,
-//   shoppingCart: shoppingCartReducer
-// });
+const rootReducer = combineReducers({
+  message: messageReducer,
+  shoppingCart: shoppingCartReducer
+});
 
 const configureStore = () => {
   const store = createStore(
     rootReducer,
-    composeWithDevTools()
+    composeWithDevTools(
+      applyMiddleware(thunk)
+    )
   );
 
   return store;
